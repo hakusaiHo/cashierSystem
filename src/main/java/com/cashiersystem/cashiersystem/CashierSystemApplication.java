54de71f2b9fbec32d048ad93549dfaef58828d24
hakusaiHo <a88666089@gmail.com>
2024-10-12 16:34:03 +0800

    生成環境架構設定

---
+++ src/main/java/com/cashiersystem/cashiersystem/CashierSystemApplication.java
@@ -0,0 +1,13 @@
+package com.cashiersystem.cashiersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashierSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashierSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cashiersystem/cashiersystem/CashierSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashiersystem.cashiersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashierSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
